080833b421261bc879e80e952fa846e49a0d3895
José Antonio González <jagonzalr@users.noreply.github.com>
2020-04-08 08:36:27 +0200

    Setup (#1)
    * Install packages
    
    * Add config files
    
    * Add webpack config
    
    * Add demo
    
    * Setup lint-staged and husky
    
    * Install lint-staged
    
    * Configure husky

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const path = require('path')
+const eslintFormatter = require('react-dev-utils/eslintFormatter')
+
+const srcPath = path.join(__dirname, './src')
+
+module.exports = {
+  target: 'web',
+  mode: 'production',
+  entry: './src/index.jsx',
+  externals: {
+    react: 'react',
+    'prop-types': 'prop-types'
+  },
+  output: {
+    path: path.resolve('lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              formatter: eslintFormatter
+            }
+          }
+        ],
+        include: [srcPath],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: require.resolve('babel-loader'),
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  },
+  resolve: {
+    mainFields: ['browser', 'main', 'module'],
+    extensions: ['.js', '.mjs', '.json', '.jsx'],
+    symlinks: false
+  }
+}
