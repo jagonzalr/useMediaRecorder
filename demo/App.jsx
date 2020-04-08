0056d21ac6f423b7708c2ae9409b8747c7cd0038
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2020-04-08 12:41:29 +0200

    Generate recording

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,14 +1,18 @@
 'use strict'
 
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 
 import { useMediaRecorder } from '../src/index'
 
 const App = () => {
-  /* eslint-disable no-unused-vars */
-  const [ref, stream, err] = useMediaRecorder()
-  /* eslint-enable no-unused-vars */
+  const recordingRef = useRef(null)
+  const [isRecording, setIsRecording] = useState(false)
   const [showPreview, setShowPreview] = useState(false)
+  const [showRecording, setShowRecording] = useState(false)
+  const [recordingUrl, setRecordingUrl] = useState(null)
+  /* eslint-disable no-unused-vars */
+  const [setCaptureRef, data, err] = useMediaRecorder(isRecording)
+  /* eslint-enable no-unused-vars */
 
   useEffect(() => {
     if (err) {
@@ -16,39 +20,108 @@ const App = () => {
     }
   }, [err])
 
-  const onStopPreview = e => {
-    e.preventDefault()
-    setShowPreview(false)
-  }
+  useEffect(() => {
+    if (data) {
+      const blob = new Blob(data, { type: 'video/webm' })
+      const url = URL.createObjectURL(blob)
+      setRecordingUrl(url)
+    }
+  }, [data])
 
-  const onPreview = e => {
+  useEffect(() => {
+    if (showRecording && recordingUrl) {
+      recordingRef.current.src = recordingUrl
+    }
+  }, [showRecording])
+
+  const onStartPreview = e => {
     e.preventDefault()
     if (!err) {
       setShowPreview(true)
     }
   }
 
+  const onStopPreview = e => {
+    e.preventDefault()
+    setShowPreview(false)
+    setShowRecording(false)
+    setRecordingUrl(null)
+  }
+
+  const onStartRecording = e => {
+    e.preventDefault()
+    setIsRecording(true)
+  }
+
+  const onStopRecording = e => {
+    e.preventDefault()
+    setIsRecording(false)
+  }
+
+  const onShowRecording = e => {
+    e.preventDefault()
+    setShowRecording(true)
+  }
+
+  const onHideRecording = e => {
+    e.preventDefault()
+    setShowRecording(false)
+  }
+
   return (
     <Fragment>
       <h1>MediaRecorder Hook</h1>
       {err && <p style={{ color: 'red', fontSize: '1.2rem' }}>{err}</p>}
-      {showPreview ? (
-        <button onClick={onStopPreview}>Stop Preview</button>
-      ) : (
-        <button onClick={onPreview}>Preview</button>
-      )}
       <div>
-        {showPreview && (
+        {!showPreview && (
+          <button onClick={onStartPreview}>Start Preview</button>
+        )}
+        {showPreview && !isRecording && !showRecording && (
+          <button onClick={onStopPreview}>Stop Preview</button>
+        )}
+        {showPreview && !isRecording && !showRecording && (
+          <button onClick={onStartRecording}>Start Recording</button>
+        )}
+        {isRecording && (
+          <button onClick={onStopRecording}>Stop Recording</button>
+        )}
+        {recordingUrl && (
+          <Fragment>
+            {!showRecording && (
+              <button onClick={onShowRecording}>Show Recording</button>
+            )}
+            {showRecording && (
+              <button onClick={onHideRecording}>Hide Recording</button>
+            )}
+            <a href={recordingUrl} download={'recording.webm'}>
+              Download
+            </a>
+          </Fragment>
+        )}
+      </div>
+      {showPreview && !showRecording && (
+        <div>
           <video
             autoPlay
             muted
-            ref={ref}
-            id='preview'
+            ref={setCaptureRef}
+            id='capture'
             width='600'
             height='400'
           />
-        )}
-      </div>
+        </div>
+      )}
+      {showRecording && (
+        <div>
+          <video
+            controls
+            ref={recordingRef}
+            id='recording'
+            width='600'
+            height='400'
+          />
+        </div>
+      )}
     </Fragment>
   )
 }
