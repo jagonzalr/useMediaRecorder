6a8d422c9a98c3a4e568aef565f428ad6bcb6ec5
José Antonio González <jagonzalr@users.noreply.github.com>
2020-04-17 12:53:49 +0200

    Stop streaming on unload (#11)
    * Stop recording on unmount
    
    * Stop recorder
    
    * Inline if

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ export const useMediaRecorder = ({ isRecording, audioOnly = false }) => {
       if (recorder) setRecorder(null)
       if (data) setData(null)
     }
+
+    return () => {
+      if (stream.id) removeTracks(stream)
+    }
   }, [ref])
 
   useEffect(() => {
@@ -47,6 +51,10 @@ export const useMediaRecorder = ({ isRecording, audioOnly = false }) => {
       }
     } else if (recorder) {
       recorder.stop()
+    }
+
+    return () => {
+      if (recorder) recorder.stop()
     }
   }, [isRecording])
 
@@ -89,10 +97,14 @@ function removeTrack(stream, track) {
 }
 
 function removeTracks(stream) {
-  const tracks = getTracks(stream)
-  if (tracks && tracks.length > 0) {
-    tracks.forEach(track => {
-      removeTrack(stream, track)
-    })
+  try {
+    const tracks = getTracks(stream)
+    if (tracks && tracks.length > 0) {
+      tracks.forEach(track => {
+        removeTrack(stream, track)
+      })
+    }
+  } catch (err) {
+    console.error(`Error while removing tracks: ${err.message}`)
   }
 }
